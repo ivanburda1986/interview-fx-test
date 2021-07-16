b14dcd8c81df69d2fea8e75d7601dec4d8b3dfc7
ivanburda1986 <burda.ivan@gmail.com>
2021-07-16 16:04:19 +0200

    outlined the error handling

--- src/api/api.js
+++ src/api/api.js
@@ -2,6 +2,11 @@
 export async function loadServerFXPairs(){
     const response = await fetch ('https://run.mocky.io/v3/c88db14a-3128-4fbd-af74-1371c5bb0343');
     const fxPairs = await response.json();
-    console.log(fxPairs);
-    return fxPairs;
+    if(response.ok){
+        console.log(fxPairs);
+        return fxPairs;
+    } else{
+        //Propagate the error to the redux store and have an error message in the up popping up if the store receives an error
+        console.log(response.status, response.statusText);
+    }
 }
