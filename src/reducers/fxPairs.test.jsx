23563517606f5584ed793a6a4a5ec25130c7e73d
ivanburda1986 <burda.ivan@gmail.com>
2021-07-27 00:38:12 +0200

    defines tests for fxpairs validation

--- src/reducers/fxPairs.test.jsx
+++ src/reducers/fxPairs.test.jsx
@@ -3,5 +3,6 @@ import { validateFXPairs, filterFXByCodeAndName } from "./fxPairs";
 describe("Returns only foreign-currency-rate pairs which are valid for the purpose of the app", () => {
   test("Excludes currencies with a code that does not have 3 characters", () => {});
   test("Excludes currencies with a missing name", () => {});
-  test("Excludes currencies with ", () => {});
+  test("Excludes currencies with missing exchange rates", () => {});
+  test("Excludes currencies with a missing middle rate", () => {});
 });
