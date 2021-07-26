b8ed340c609eda0150091a80e185ef38e7a5226c
ivanburda1986 <burda.ivan@gmail.com>
2021-07-27 01:29:17 +0200

    working on tests

--- src/reducers/fxPairs.test.jsx
+++ src/reducers/fxPairs.test.jsx
@@ -1,7 +1,9 @@
 import { validateFXPairs, filterFXByCodeAndName } from "./fxPairs";
 
 describe("Returns only foreign-currency-rate pairs which are valid for the purpose of the app", () => {
-  test("Excludes currencies with a code that does not have 3 characters", () => {});
+  test("Excludes currencies with a code that does not have 3 characters", () => {
+    expect(validateFXPairs(CZK)).toBe("CZK");
+  });
   test("Excludes currencies with a missing name", () => {});
   test("Excludes currencies with missing exchange rates", () => {});
   test("Excludes currencies with a missing middle rate", () => {});
@@ -32,12 +34,16 @@ const MISSING_MIDDLE_RATE = {
 };
 
 //Valid currency
-const CZK = {
-  currency: "CZK",
-  precision: 2,
-  nameI18N: "Czech Koruna",
-  denominations: [5000, 2000, 1000, 500, 200, 100],
-  exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
-  banknoteRate: { buy: 24.55, middle: 25.9, sell: 27.25, indicator: 0, lastModified: "2018-11-06T23:00:00Z" },
-  flags: ["provided"],
-};
+const CZK = [
+  {
+    currency: "CZK",
+    precision: 2,
+    nameI18N: "Czech Koruna",
+    denominations: [5000, 2000, 1000, 500, 200, 100],
+    exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
+    banknoteRate: { buy: 24.55, middle: 25.9, sell: 27.25, indicator: 0, lastModified: "2018-11-06T23:00:00Z" },
+    flags: ["provided"],
+  },
+];
+
+//https://stackoverflow.com/questions/39193533/how-to-test-json-parse-in-jest
