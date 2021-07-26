849baf6bef5acdd9d75beeab7e33cdb38c1e41ae
ivanburda1986 <burda.ivan@gmail.com>
2021-07-26 23:53:19 +0200

    filtering function improvements

--- src/reducers/fxPairs.js
+++ src/reducers/fxPairs.js
@@ -1,27 +1,25 @@
 import { LOAD_FXPAIRS, FILTER_FXPAIRS } from "../actions/fxPairs";
 
-//Helping function - place them to an external file later on
+//Helping functions
+
 const validateFXPairs = function (fxPairs) {
   return fxPairs.filter((pair) => pair.currency.replace(/\s+/g, "").length === 3 && pair.nameI18N !== undefined && pair.exchangeRate !== undefined);
 };
 
 const filterFXByCodeAndName = function ({ fxPairs, filterString }) {
-  let regexSpace = /%20/gi;
   let regexHash = /#/gi;
-  let filterBy = filterString.replaceAll(regexSpace, "").replaceAll(regexHash, "").toUpperCase();
-  //console.log("-----------------------------------------------------------");
-  //console.log("FilterBy:", filterBy);
+  let regexURLSpace = /%20/gi;
+  let filterBy = filterString.replaceAll(regexURLSpace, "").replaceAll(regexHash, "").toLowerCase();
 
   let results = Object.values(fxPairs).filter((pair) => {
-    let relevantCurrencyCodePart = pair.currency.toUpperCase().replaceAll(regexSpace, "").slice(0, filterBy.length);
-    let relevantCurrencyNamePart = pair.nameI18N.toUpperCase().split(" ");
-    //console.log(relevantCurrencyCodePart);
-    //console.log(relevantCurrencyNamePart);
-    return relevantCurrencyCodePart === filterBy || relevantCurrencyNamePart.some((word) => word.slice(0, filterBy.length) === filterBy);
+    let relevantCurrencyCodePart = pair.currency.toLowerCase().slice(0, filterBy.length);
+    let currencyNameIndividualWords = pair.nameI18N.toLowerCase().split(" ");
+    return relevantCurrencyCodePart === filterBy || currencyNameIndividualWords.some((word) => word.slice(0, filterBy.length) === filterBy);
   });
   return results;
 };
 
+//Reducer
 export default function fxPairs(state = { data: {} }, action) {
   switch (action.type) {
     case LOAD_FXPAIRS:
