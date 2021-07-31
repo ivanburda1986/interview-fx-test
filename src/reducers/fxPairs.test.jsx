e38424b08e3bb79231ee531cb3f12c80ca813f01
ivanburda1986 <burda.ivan@gmail.com>
2021-07-31 15:24:37 +0200

    finished tests for currency validation

--- src/reducers/fxPairs.test.jsx
+++ src/reducers/fxPairs.test.jsx
@@ -2,54 +2,69 @@ import { validateFXPairs, filterFXByCodeAndName } from "./fxPairs";
 
 describe("Returns only foreign-currency-rate pairs which are valid for the purpose of the app", () => {
   test("Excludes currencies with a code that does not have 3 characters", () => {
-    expect(validateFXPairs([INVALID_CODE1, INVALID_CODE2, INVALID_CODE3, INVALID_CODE4, MXN, CZK])).toEqual([MXN, CZK]);
+    expect(validateFXPairs([INVALID_CODE1, INVALID_CODE2, INVALID_CODE3, INVALID_CODE4, VALID])).toEqual([VALID]);
   });
-  test("Excludes currencies with a missing name", () => {});
-  test("Excludes currencies with missing exchange rates", () => {});
-  test("Excludes currencies with a missing middle rate", () => {});
+  test("Excludes currencies with a missing name", () => {
+    expect(validateFXPairs([MISSING_NAME, VALID])).toEqual([VALID]);
+  });
+  test("Excludes currencies with missing exchange rates", () => {
+    expect(validateFXPairs([MISSING_EXCHANGE_RATES, VALID])).toEqual([VALID]);
+  });
+  test("Excludes currencies with a missing middle rate", () => {
+    expect(validateFXPairs([MISSING_MIDDLE_RATE, VALID])).toEqual([VALID]);
+  });
 });
 
-//TEST DATA --
-
+//TEST DATA
 //Invalid currency code
-const INVALID_CODE1 = { currency: "   ", precision: 2, banknoteRate: { buy: 882.5, middle: 882.5, sell: 882.5, indicator: 0, lastModified: "2008-08-06T22:00:00Z" }, flags: ["provided"] };
-const INVALID_CODE2 = { currency: "EU", precision: 2, banknoteRate: { buy: 882.5, middle: 882.5, sell: 882.5, indicator: 0, lastModified: "2008-08-06T22:00:00Z" }, flags: ["provided"] };
-const INVALID_CODE3 = { currency: "EURO", precision: 2, banknoteRate: { buy: 882.5, middle: 882.5, sell: 882.5, indicator: 0, lastModified: "2008-08-06T22:00:00Z" }, flags: ["provided"] };
-const INVALID_CODE4 = { currency: "", precision: 2, banknoteRate: { buy: 882.5, middle: 882.5, sell: 882.5, indicator: 0, lastModified: "2008-08-06T22:00:00Z" }, flags: ["provided"] };
+const INVALID_CODE1 = {
+  currency: "   ",
+  nameI18N: "Middle Earth Dollar",
+  exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
+};
+const INVALID_CODE2 = {
+  currency: "ME",
+  nameI18N: "Middle Earth Dollar",
+  exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
+};
+const INVALID_CODE3 = {
+  currency: "MEDL",
+  nameI18N: "Middle Earth Dollar",
+  exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
+};
+const INVALID_CODE4 = {
+  currency: "",
+  nameI18N: "Middle Earth Dollar",
+  exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
+};
 
 //Missing name
-const CDF = { currency: "CDF", precision: 2, flags: ["provided"] };
+const MISSING_NAME = {
+  currency: "MED",
+  exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
+};
 
 //Missing exchange rates
-const LSL = { currency: "LSL", precision: 2, nameI18N: "Lesotho Loti" };
+const MISSING_EXCHANGE_RATES = {
+  currency: "MED",
+  nameI18N: "Middle Earth Dollar",
+};
 
 //With missing exchange rate middle
 const MISSING_MIDDLE_RATE = {
-  currency: "ABC",
-  precision: 0,
-  nameI18N: "Imaginary currency",
-  exchangeRate: { buy: 1205.0, sell: 1355.0, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
-  banknoteRate: { buy: 1080.0, sell: 1480.0, indicator: 0, lastModified: "2018-11-06T23:00:00Z" },
-  flags: ["provided"],
+  currency: "MED",
+  nameI18N: "Middle Earth Dollar",
 };
+
 //Valid currency
-const CZK = {
+const VALID = {
   currency: "CZK",
-  precision: 2,
   nameI18N: "Czech Koruna",
-  denominations: [5000, 2000, 1000, 500, 200, 100],
   exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
-  banknoteRate: { buy: 24.55, middle: 25.9, sell: 27.25, indicator: 0, lastModified: "2018-11-06T23:00:00Z" },
-  flags: ["provided"],
 };
 
-const MXN = {
+const VALID2 = {
   currency: "MXN",
-  precision: 2,
   nameI18N: "Mexican Peso",
-  exchangeRate: { buy: 22.38, middle: 22.98, sell: 23.58, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
-  banknoteRate: { buy: 21.1, middle: 22.6, sell: 24.1, indicator: 0, lastModified: "2018-11-06T23:00:00Z" },
-  flags: ["provided"],
+  exchangeRate: { buy: 25.575, middle: 25.925, sell: 26.275, indicator: 0, lastModified: "2018-11-08T23:00:00Z" },
 };
-
-//https://stackoverflow.com/questions/39193533/how-to-test-json-parse-in-jest
