0a02764b4d50df68f8fb700d652d43eba6c02536
ivanburda1986 <burda.ivan@gmail.com>
2021-08-29 01:22:18 +0200

    added api fallback mocked data

--- src/actions/fxPairs.js
+++ src/actions/fxPairs.js
@@ -1,4 +1,6 @@
 import { loadServerFXPairs } from "../api/api";
+import ApiMocked from "../api/api-mocked.json";
+
 export const LOAD_FXPAIRS = "LOAD_FXPAIRS";
 
 function loadFXPairs(fxPairs) {
@@ -10,8 +12,13 @@ function loadFXPairs(fxPairs) {
 
 export function handleLoadServerFXPairs() {
   return (dispatch) => {
-    loadServerFXPairs().then((data) => {
-      dispatch(loadFXPairs(data));
-    });
+    loadServerFXPairs()
+      .then((data) => {
+        dispatch(loadFXPairs(data));
+      })
+      .catch(({ message }) => {
+        console.log("Loading live FX data from the server failed. Using a local mockfile instead.");
+        dispatch(loadFXPairs(ApiMocked));
+      });
   };
 }
